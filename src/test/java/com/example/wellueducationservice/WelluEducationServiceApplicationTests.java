bfaf9d100354379b13c04b4c2aa9bf0bced6bb58
Rola Hadidy <rolatotta2004@gmail.com>
2026-04-19 21:21:38 +0200

    initial commit

---
+++ src/test/java/com/example/wellueducationservice/WelluEducationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wellueducationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelluEducationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
